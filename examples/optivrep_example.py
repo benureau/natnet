6a5ee3abe2af9dabb114c9fd1e64ee634b0c88e6
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-03-24 15:20:01 +0100

    remove unused import

--- examples/optivrep_example.py
+++ examples/optivrep_example.py
@@ -6,17 +6,17 @@ optivrep_example.py
 Created by Paul Fudal on 2014-01-27.
 Copyright (c) 2014 __MyCompanyName__. All rights reserved.
 """
-
 import time
 import numpy as np
+
 from surrogates.vrepsim import vrepcom
 from surrogates.stemsim import optivrepar
+
 import env
 from natnet import FrameBuffer
-import treedict
 
 def main(move_time=10.0):
-    
+
     fb = FrameBuffer(move_time)
     fb.track_only()
     start = time.time()
@@ -24,7 +24,7 @@ def main(move_time=10.0):
     fb.stop_tracking()
     end = time.time()
     trajectory = fb.tracking_period(start, end)
-    
+
     ovar = optivrepar.OptiVrepAR()
     ovar.execute(trajectory)
     ovar.close()
